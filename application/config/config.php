85584e2529609c08d8240c722e87cc2878727717
estigadotnet <project.estiga@gmail.com>
2020-07-26 13:12:32 +0700

    .

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = "https://siks.salinalintas.com/"; //'http://localhost/siks/';
+$config['base_url'] = "http://siks.salinalintas.com/"; //'http://localhost/siks/';
 
 /*
 |--------------------------------------------------------------------------
