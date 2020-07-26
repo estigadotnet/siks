61fc2d5073f8933b7d858176f0940135dda27898
estigadotnet <project.estiga@gmail.com>
2020-07-26 13:54:17 +0700

    .

--- index.php
+++ index.php
@@ -53,7 +53,23 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+
+	switch ($_SERVER["HTTP_HOST"]) {
+		case "localhost":
+			$env = "development";
+			break;
+
+		case "http://siks.salinalintas.com":
+			$env = "production";
+			break;
+
+		default:
+			$env = "development";
+			break;
+	}
+
+	// define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : $env);
 
 /*
  *---------------------------------------------------------------
